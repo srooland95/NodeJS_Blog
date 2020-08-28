b2f313884f91cfee28ba1e251ce2093d13e4f266
srooland95 <srooland95@gmail.com>
2020-08-28 21:53:06 +0200

    test and set db models

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
-const dbURI = 'mongodb+srv://srooland95:Slash1014@learn-sandbox.hbwgw.mongodb.net/<dbname>?retryWrites=true&w=majority';
-mongoose.connect(dbURI);
+const dbURI = 'mongodb+srv://srooland95:Slash1014@learn-sandbox.hbwgw.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
-// listen for request
-app.listen(3000);
 
 // register view engine
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 // middleware and statics
 app.use(express.static('public'));
@@ -20,18 +21,23 @@ app.use(morgan('dev'));
 
 //  get views
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-
-  res.render('index', { title: 'Blogs', blogs});
+  res.redirect('/blogs');
 })
 
 app.get('/about', (req,res) => {
   res.render('about', { title: 'About'});
 })
+
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: 'Blogs', blogs: result })
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.get('/blog/create', (req, res) => {
   res.render('create', { title: 'Create blog'});
