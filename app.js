177894235bbf1be54ed2e2555be7b2a9d989fc6c
srooland95 <srooland95@gmail.com>
2020-08-29 15:38:28 +0200

    add get post and delete methods

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
+const { json } = require('express');
 
 // express app
 const app = express();
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');
 
 // middleware and statics
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 //  get views
@@ -38,6 +40,38 @@ app.get('/blogs', (req, res) => {
       console.log(err);
     });
 });
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+})
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  
+  Blog.findByIdAndDelete(id)
+    .then((result) => res.json({ redirect: '/blogs'}))
+    .catch((err) => console.log(err));
+});
+
 
 app.get('/blog/create', (req, res) => {
   res.render('create', { title: 'Create blog'});
